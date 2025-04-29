3d77bc49f8a00642c1f636c3681120a89c8301ac
yoneasa <yoneasa0920@gmail.com>
2025-04-29 21:37:31 +0900

    feat: add api

---
+++ api/src/functions/message.js
@@ -0,0 +1,9 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        return { body: `Hello, from the API!` };
+    }
+});
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const value = 'World';
   const [count, setCount] = useState(0);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
 
   const clickHandler = () => {
     setCount((count) => count + 1);
@@ -15,6 +23,7 @@ function App() {
       <button onClick={clickHandler}>CLICK</button>
       <div>You clicked the button {count} times</div>
       <p style={{ color: 'red' }}>これは赤い文字です！</p>
+      <div>DATA: {data}</div>
     </>
   );
 }

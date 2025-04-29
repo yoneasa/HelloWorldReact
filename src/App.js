c1fce7072db02e81dc1ec4ae3c90d7356445d63b
yoneasa <yoneasa0920@gmail.com>
2025-04-29 21:10:38 +0900

    feat: add button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import useState from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const [count, setCount] = useState(0);
+
+  const clickHandler = () => {
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <>
+      <div>Hello {value}</div>
+      <button onClick={clickHandler}>CLICK</button>
+      <div>You clicked the button {count} times</div>
+    </>
+  );
 }
 
 export default App;

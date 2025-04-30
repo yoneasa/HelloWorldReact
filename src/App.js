a520fc6c0211e5f3f2ae29725c562451f05545a3
yoneasa <yoneasa0920@gmail.com>
2025-04-30 21:49:23 +0900

    feat: app func call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     (async function () {
@@ -9,10 +10,22 @@ function App() {
       const data = await response.json(); // JSONとして取得
       console.log(data.message); // "Hello, from the API!" が表示される
       setData(data.message);
+
+      fetch("https://your-function-app.azurewebsites.net/api/your-function", {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(message => setMessage(message));
+    
     })();
   }, []);
 
-  return <div>{data}</div>;
+  return(
+    <>
+      <div>{data}</div>
+      <div>{message}</div>
+    </>
+  );
 }
 
 export default App;
